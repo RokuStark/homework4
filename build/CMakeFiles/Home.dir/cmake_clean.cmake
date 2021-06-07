d14e212dbaba01623abf9920fec01bcf8c41cb53
rokustark <rokustark@gmail.com>
2021-06-07 12:19:29 +0300

    add other files

---
+++ build/CMakeFiles/Home.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Home.dir/main.cpp.o"
+  "Home"
+  "Home.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Home.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
